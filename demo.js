04ccd9babcc7fece7231babf66e2a7379c93bda5
Gopi-maker <gopibollepalli6@gmail.com>
2023-05-14 13:34:03 +0530

    selectAll

--- demo.js
+++ demo.js
@@ -14,15 +14,28 @@ item.style.fontWeight='bold';
 item.style.color='green';
 //getElementByClassName
 var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor="green";
+//items[2].style.backgroundColor="orange";
 for(var i=0;i<items.length;i++){
 items[i].style.fontWeight="bold";
 }
 //getElementByTagName
-var li = document.getElementsByTagName('li');
-li[4].textContent='Gopi';
+//var li = document.getElementsByTagName('li');
+//li[4].textContent='Gopi';
 //var list=document.getElementsByClassName('list-group-item');
 //for(var i=0;i<list.length;i++){
     //list[i].classList.add('not-in-list') ;
 //}
 
+//querySelector
+//var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.backgroundColor="green";
+//var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+//thirdItem.style.display='none';
+
+//querySelectorAll
+var Item2=document.querySelectorAll('.list-group-item');
+Item2[1].style.color="green";
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
