10401e83901b85586a112f4fdd406c2a52d3819f
Gopi-maker <gopibollepalli6@gmail.com>
2023-05-15 14:02:05 +0530

    newLi

--- demo.js
+++ demo.js
@@ -43,6 +43,67 @@
 //traversing Dom
 var itemList=document.querySelector('#items')
 //parentNode
-console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor="#f4f4f4";
+//console.log(itemList.parentNode);
+//itemList.parentNode.style.backgroundColor="#f4f4f4";
+//console.log(itemList.parentNode.parentNode.parentNode);
 
+//parentElement
+//console.log(itemList.parentElement);
+//itemList.parentElement.style.backgroundColor="#f4f4f4";
+//console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+//console.log(itemList.childNodes);
+
+//console.log(itemList.children);
+//console.log(itemList.children[1]);
+//itemList.children[1].style.backgroundColor='orange';
+
+//firstChild
+//console.log(itemList.firstChild);
+//firstElementChild
+//console.log(itemList.firstElementChild);
+//itemList.firstElementChild.textContent="Hello";
+
+//lastChild
+//console.log(itemList.lastChild);
+//lastElementChild
+//console.log(itemList.lastElementChild);
+//itemList.lastElementChild.textContent="Hello 4";
+//nextSibling
+//console.log(itemList.nextSibling);
+//nextElementSibling
+//console.log(itemList.nextElementSibling);
+
+//previousSibling
+//console.log(itemList.previousSibling);
+//previousElementSibling
+//console.log(itemList.previousElementSibling);
+//itemList.previousElementSibling.style.color='green';
+
+//createElement
+//create newDiv Element
+
+var newDiv=document.createElement('div');
+//Add class
+newDiv.className='hello';
+//add id
+newDiv.id='hello1';
+//add attribute
+newDiv.setAttribute('title','hello Div');
+//create text node
+var newDivText =document.createTextNode("Hello");
+//add text div
+newDiv.appendChild(newDivText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+container.insertBefore(newDiv,h1);
+
+var newLi=document.createElement('li');
+newLi.className='list-group-item';
+var listText=document.createTextNode("Hello");
+newLi.appendChild(listText);
+var cont=document.querySelector('#items');
+cont.insertBefore(newLi,cont.children[0]);
